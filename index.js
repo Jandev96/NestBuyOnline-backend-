d64f6b6bc52bec6723676cf3606e123463ce377a
jandev <jandev66@gmail.com>
2025-03-30 15:31:17 +0530

    cors updated

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ import cookieParser from "cookie-parser"
 import cors from 'cors'
 
 
-app.use(cors(
-    {
-        origin:'http://localhost:5173',
-        credentials:true,
-        methods:['GET',"POST","PUT","DELETE","OPTION"]
-    }
-))
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://nestbuyonline-frontend.vercel.app"
+        ], 
+        credentials: true, 
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    })
+);
 
 app.use(express.json())
 app.use(cookieParser())
