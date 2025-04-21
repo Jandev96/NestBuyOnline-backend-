77a13a1377077f0041e0034fbb14fdbbca08cfde
jandev <jandev66@gmail.com>
2025-04-22 00:25:36 +0530

    cors updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://nestbuyonline-frontend.vercel.app"
+            "https://nestbuyonlinenew.vercel.app"
         ], 
         credentials: true, 
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
