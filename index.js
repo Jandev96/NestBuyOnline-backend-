79a651ca35a884303a2fe6d07ef8fff7b2902d13
jandev <jandev66@gmail.com>
2025-04-22 00:47:58 +0530

    revertd back

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ import cookieParser from "cookie-parser"
 import cors from 'cors'
 
 
-app.use(cors())
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://nestbuyonlinenew.vercel.app"
+        ], 
+        credentials: true, 
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    })
+);
 
 app.use(express.json())
 app.use(cookieParser())
