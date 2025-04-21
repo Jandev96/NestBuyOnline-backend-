6cf9891fe82168e6092a3da9aecd88419ede5054
jandev <jandev66@gmail.com>
2025-04-22 00:45:10 +0530

    cors changed

--- index.js
+++ index.js
@@ -8,16 +8,7 @@ import cookieParser from "cookie-parser"
 import cors from 'cors'
 
 
-app.use(
-    cors({
-        origin: [
-            "*"
-            
-        ], 
-        credentials: true, 
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-    })
-);
+app.use(cors())
 
 app.use(express.json())
 app.use(cookieParser())
